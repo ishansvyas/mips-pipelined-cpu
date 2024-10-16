828a15bea07c4cb2b61f1107130fd278401a0df3
Ishan Vyas <isv4@duke.edu>
2024-10-16 12:08:51 -0400

    Debugging divider, realized non-restoring handles negatives poorly

--- cp3-multdiv/multdiv/divider.v
+++ cp3-multdiv/multdiv/divider.v
@@ -4,6 +4,8 @@ module divider (
 	clock, 
 	data_result, data_exception, data_resultRDY);
 
+    //// NOTE: NON-RESTORING ONLY WORKS FOR NON-NEGATIVES
+
     // IO instantiation
     input [31:0] data_operandA, data_operandB;
     input ctrl_MULT, ctrl_DIV, clock;
@@ -15,14 +17,20 @@ module divider (
 
     // wires because thats how ece works
     wire [63:0] albert, bartholomew, candice, dexter;
-    wire [31:0] vermouth;
+    wire [31:0] vermouth, data_operandA_not, data_operandB_not, data_A_in, data_B_in, dexter_not;
+    wire [2:0] Cout_extra;
 
     // Registers
-    register #(32) reg_V(.out(vermouth), .in(data_operandB), .clk(clock), .en(ctrl_DIV), .clr(ctrl_MULT));
+    register #(32) reg_V(.out(vermouth), .in(data_B_in), .clk(clock), .en(ctrl_DIV), .clr(ctrl_MULT));
     register #(64) reg_RQ(.out(bartholomew), .in(albert), .clk(clock), .en(clock), .clr(ctrl_MULT));
 
+
     // ABOVE RQ (EA)
-    assign albert = ctrl_DIV ? {32'd0, data_operandA} : dexter;
+    cla_outer_32 negateA(.data_operandA(~data_operandA), .data_operandB(32'd1), .Cin(1'b0), .data_result(data_operandA_not), .Cout(Cout_extra[0]));
+    cla_outer_32 negateB(.data_operandA(~data_operandB), .data_operandB(32'd1), .Cin(1'b0), .data_result(data_operandB_not), .Cout(Cout_extra[1]));
+    assign data_A_in = data_operandA[31] ? data_operandA_not : data_operandA;
+    assign data_B_in = data_operandB[31] ? data_operandB_not : data_operandB;
+    assign albert = ctrl_DIV ? {32'd0, data_A_in} : dexter;
 
     // (AB)
     // register instantiation via above
@@ -46,7 +54,9 @@ module divider (
      
     // FINAL (test comment)
     // assign data_result[63:32] = dexter[63] ? dplus : dexter[63:32]; // FOR REMAINDER BIT
-    assign data_result = dexter[31:0];
+    cla_outer_32 negateRES(.data_operandA(~dexter[31:0]), .data_operandB(32'd1), .Cin(1'b0), .data_result(dexter_not), .Cout(Cout_extra[2]));
+    assign data_result = (data_operandA[31] ^ data_operandB[31]) ? dexter_not : dexter[31:0];
+
 
     /////////////      TIMING (possibly use single counter instantiated in multdiv module rather than 1 each)
     wire [5:0] counter, data_ready_32;
