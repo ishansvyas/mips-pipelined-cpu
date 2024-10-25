d1e173ba49ebbdc8f468fbeb9ed4b8a287195105
ishansvyas <ishan.vyas@duke.edu>
2024-10-24 20:44:40 -0400

    full FDX with branching. no writing yet

--- cp4-processor/alu/sign_extend.v
+++ cp4-processor/alu/sign_extend.v
@@ -1,4 +1,4 @@
-module sign_extend(out, in);
+module sign_extend_17_32(out, in);
     input [16:0] in;
     output [31:0] out;
     assign out[16:0] = in;
--- cp4-processor/processor.v
+++ cp4-processor/processor.v
@@ -66,19 +66,27 @@ module processor(
     wire not_clock;
     assign not_clock = !clock;
 
-    ////////// START OF FETCH 
+    ////////// START OF FETCH //////////
 
     wire [31:0] pc_in, pc_plus_one;
     
 	register program_counter(.out(address_imem), .in(pc_in), .clk(clock), .en(1'b1), .clr(reset));
     alu_cla_outer pc_plus_one_adder(.data_operandA(address_imem), .data_operandB(32'd1), .Cin(1'd0), .data_result(pc_plus_one), .Cout());
-    assign pc_in = pc_plus_one;
+    
+    // next PC calculation [SEE branching IN EXECUTE FOR LOGIC/DESTINATION CALCULATION]
+    mux4 pc_branch_control(.out(pc_in), .select(pc_branch_control), .in0(pc_plus_one), .in1(pc_plus_N), .in2(decode_B_out), .in3(pc_T));
+    /*      PC+1:       pc_plus_one
+            PC+1+N:     pc_plus_N
+            $rd:        decode_B_out
+            T:          pc_T
+    */
 
-    ////////// END OF FETCH 
+
+    ////////// END OF FETCH //////////
     wire [31:0] fetch_PC_out, fetch_INSN_out;
     register fetch_PC(.out(fetch_PC_out), .in(pc_in), .clk(not_clock), .en(1'b1), .clr(reset));
     register fetch_INSN(.out(fetch_INSN_out), .in(q_imem), .clk(not_clock), .en(1'b1), .clr(reset));
-    ////////// START OF DECODE
+    ////////// START OF DECODE //////////
 
     wire [31:0] data_writeReg_w;
     // assign inputs to regfile, output of regfile in latch
@@ -87,38 +95,60 @@ module processor(
     assign ctrl_writeEnable = 1'b0; // NOTE: CURRENTLY DISABLED
     assign ctrl_writeReg = fetch_INSN_out[26:22];
     assign ctrl_readRegA = fetch_INSN_out[21:17];
-    assign ctrl_readRegB = fetch_INSN_out[16:12];
+    assign ctrl_readRegB = !(|(fetch_INSN_out[31:27]^5'b10110)) ? 5'b11110 : fetch_INSN_out[16:12]; // BEX LOGIC
 	assign data_writeReg = data_writeReg_w;
 
-    ////////// END OF DECODE
+    ////////// END OF DECODE //////////
     wire [31:0] decode_PC_out, decode_A_out, decode_B_out, decode_INSN_out;  
     register decode_PC(.out(decode_PC_out), .in(fetch_PC_out), .clk(not_clock), .en(1'b1), .clr(reset));
     register decode_A(.out(decode_A_out), .in(data_readRegA), .clk(not_clock), .en(1'b1), .clr(reset));
     register decode_B(.out(decode_B_out), .in(data_readRegB), .clk(not_clock), .en(1'b1), .clr(reset));
     register decode_INSN(.out(decode_INSN_out), .in(fetch_INSN_out), .clk(not_clock), .en(1'b1), .clr(reset));
-    ////////// START OF EXECUTE
+    ////////// START OF EXECUTE //////////
 
+    /// MUX for B input
+    wire is_R_type;
+    assign is_R_type = !(|decode_INSN_out[31:27]);
+    wire [31:0] sign_extend_immed_out, ALU_input_B;
+    sign_extend_17_32 extender(.out(sign_extend_immed_out), .in(decode_INSN_out[31:27]));
+    assign ALU_input_B = is_R_type ? decode_B_out : sign_extend_immed_out;
+
+    // ALU; NOTE: isLT, isNE are unused. OVF undefined too but that's ok?
+    wire [31:0] alu_out;
+    wire alu_isNE, alu_isLT;
     alu execute_alu(
-            .data_operandA(), .data_operandB(),
-            .ctrl_ALUopcode(), .ctrl_shiftamt(),
-            .data_result(),
-            .isNotEqual(), .isLessThan(), .overflow());    
-/* WIRE LENGTHS: 
-    input [31:0] data_operandA, data_operandB;
-    input [4:0] ctrl_ALUopcode, ctrl_shiftamt;
+            .data_operandA(decode_A_out), .data_operandB(ALU_input_B),
+            .ctrl_ALUopcode(decode_INSN_out[6:2]), .ctrl_shiftamt(decode_INSN_out[11:7]),
+            .data_result(alu_out),
+            .isNotEqual(alu_isNE), .isLessThan(alu_isLT), .overflow());    
 
-    output [31:0] data_result;
-    output isNotEqual, isLessThan, overflow;
-*/
+    // branching: destination calculation
+    wire [31:0] pc_plus_N, pc_T;
+    alu_cla_outer pc_plus_N(.data_operandA(decode_PC_out), .data_operandB(sign_extend_immed_out), .Cin(1'b0), .data_result(pc_plus_N), .Cout());
+    assign pc_T[26:0] = decode_INSN_out[26:0];
+    assign pc_T[31:27] = {5{decode_INSN_out[26]}};
+
+    // branching: logic calculation
+    wire [4:0] decode_out_opcode;
+    wire take_pc_N, take_T, take_rd;
+    assign decode_out_opcode = decode_INSN_out[31:27]; 
+    assign take_pc_N = (!(|(decode_out_opcode^5'b00010)) && (alu_isNE)) || (!(|(decode_out_opcode^5'b00110)) && (alu_isLT));
+    assign take_T = (!(|(decode_out_opcode^5'b00011))) || (!(|(decode_out_opcode^5'b10110)) && (|(decode_B_out)));
+    assign take_rd = (!(|(decode_out_opcode^5'b00100)));
+
+    // mux assignment: {00,01,10,11} = {+1, +N+1, T, $rd}
+    wire [1:0] pc_branch_control;
+    assign pc_branch_control[0] = take_pc_N || take_rd;
+    assign pc_branch_control[1] = take_T || take_rd;
 
 
-    ////////// END OF EXECUTE
+
+    ////////// END OF EXECUTE //////////
     wire [31:0] execute_pc_out, execute_O_out, execute_B_out, execute_INSN_out;
-    register execute_pc(.out(execute_pc_out), .in(decode_PC_out), .clk(not_clock), .en(1'b1), .clr(reset));
-    register execute_O(.out(execute_O_out), .in(/*   TO ASSIGN!!!!    */), .clk(not_clock), .en(1'b1), .clr(reset));
+    register execute_O(.out(execute_O_out), .in(alu_out), .clk(not_clock), .en(1'b1), .clr(reset));
     register execute_B(.out(execute_B_out), .in(decode_B_out), .clk(not_clock), .en(1'b1), .clr(reset));
     register execute_INSN(.out(execute_INSN_out), .in(decode_INSN_out), .clk(not_clock), .en(1'b1), .clr(reset));
-    ////////// START OF MEMORY
+    ////////// START OF MEMORY //////////
 
 
 	/* END CODE */
