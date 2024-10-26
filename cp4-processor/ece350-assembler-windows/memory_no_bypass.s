c9064bca7699f82d72409e414dac7bb786b991d0
ishansvyas <ishan.vyas@duke.edu>
2024-10-26 12:27:08 -0400

    memory files for memory test ahaha

--- cp4-processor/ece350-assembler-windows/memory_no_bypass.s
+++ cp4-processor/ece350-assembler-windows/memory_no_bypass.s
@@ -11,10 +11,10 @@ addi $2, $0, 21	# r2 = 21
 sub $3, $0, $3	# r3 = -1
 sub $4, $0, $4	# r4 = -35
 nop 
-nop 			# Positive Value Tests
-add $5, $2, $1	# r5 = r2 + r1 = 24
-sub $6, $2, $1	# r6 = r2 - r1 = 18
-and $7, $2, $1	# r7 = r2 & r1 = 1
-or $8, $2, $1 	# r8 = r2 | r1 = 23
-sll $9, $1, 4 	# r9 = r1 << 4 = 48
-sra $10, $2, 2	# r10 = r2 >> 2 = 5
+nop 			# Load/Store Tests
+sw $1, 1($0) 	# mem[1] = r1 = 3
+sw $2, 2($0) 	# mem[2] = r2 = 21
+sw $3, 0($1) 	# mem[r1] = r3 = -1 (should be mem[3])
+lw $16, 1($0) 	# r16 = mem[1] = 3
+lw $17, 2($0) 	# r17 = mem[2] = 21
+lw $18, 0($1) 	# r18 = mem[3] = -1
