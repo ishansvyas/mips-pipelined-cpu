7889b2bbd207735de5c2f856d59cb79d7c5202c2
ishansvyas <ishan.vyas@duke.edu>
2024-10-30 13:10:48 -0400

    RESET TO BABY CPU PROGRESS

--- cp4-processor/processor.v
+++ cp4-processor/processor.v
@@ -65,13 +65,12 @@ module processor(
 
     wire not_clock;
     assign not_clock = !clock;
-    wire [31:0] nop = 32'd0; 
 
     ////////// START OF FETCH //////////
 
-    wire [31:0] pc_in, pc_plus_one, fetch_INSN_in;
+    wire [31:0] pc_in, pc_plus_one;
     
-	register program_counter(.out(address_imem), .in(pc_in), .clk(clock), .en(1'b1), .clr(reset));
+	register program_counter(.out(address_imem), .in(pc_in), .clk(not_clock), .en(1'b1), .clr(reset));
     alu_cla_outer pc_plus_one_adder(.data_operandA(address_imem), .data_operandB(32'd1), .Cin(1'd0), .data_result(pc_plus_one), .Cout());
     
     // next PC calculation [SEE branching IN EXECUTE FOR LOGIC/DESTINATION CALCULATION]
@@ -82,52 +81,40 @@ module processor(
             T:          pc_T
     */
 
-    // nop pre-INSN latch
-    assign fetch_INSN_in = take_pc_N ? nop : q_imem;
 
     ////////// END OF FETCH //////////
     wire [31:0] fetch_PC_out, fetch_INSN_out;
     register fetch_PC(.out(fetch_PC_out), .in(pc_in), .clk(not_clock), .en(1'b1), .clr(reset));
-    register fetch_INSN(.out(fetch_INSN_out), .in(fetch_INSN_in), .clk(not_clock), .en(1'b1), .clr(reset));
+    register fetch_INSN(.out(fetch_INSN_out), .in(q_imem), .clk(not_clock), .en(1'b1), .clr(reset));
     ////////// START OF DECODE //////////
 
-    wire [1:0] mux_ctrl_readRegB;
-    assign mux_ctrl_readRegB[1] = !(|(fetch_INSN_out[31:27]^5'b10110)); // BEX condition
-    assign mux_ctrl_readRegB[0] = !(|(fetch_INSN_out[31:27]^5'b00111)); // SW  condition
+    // assign inputs to regfile, output of regfile in latch
 
+    /* ONLY INTERRACT WITH REGFILE VIA I/O OF PROCESSOR, NOT BY DIRECT INSTANTIAION */
     // assign ctrl_writeEnable IS DONE IN WRITEBACK STAGE
     // assign ctrl_writeReg IS DONE IN WRITEBACK STAGE
     assign ctrl_readRegA = fetch_INSN_out[21:17];
-    mux4 #(5) ctrl_readRegB_logic(.out(ctrl_readRegB), .select(mux_ctrl_readRegB), .in0(in_0), .in1(fetch_INSN_out[26:22]), .in2(5'd0), .in3(5'b11110));
+    assign ctrl_readRegB = !(|(fetch_INSN_out[31:27]^5'b10110)) ? 5'b11110 : fetch_INSN_out[16:12]; // BEX LOGIC
 	// assign data_writeReg IS DONE IN WRITEBACK STAGE
 
-    // above assumes R type instruction: fetch_INSN_out[16:12]
-    wire branch_cond;
-    wire [4:0] in_0;
-    assign branch_cond = !(|(fetch_INSN_out[31:27]^5'b00110)) || !(|(fetch_INSN_out[31:27]^5'b00010));
-    assign in_0 = branch_cond ? fetch_INSN_out[26:22] : fetch_INSN_out[16:12];
-
-    assign decode_INSN_in = take_pc_N ? nop : fetch_INSN_out;
     ////////// END OF DECODE //////////
-    wire [31:0] decode_PC_out, decode_A_out, decode_B_out, decode_INSN_out, decode_INSN_in;  
+    wire [31:0] decode_PC_out, decode_A_out, decode_B_out, decode_INSN_out;  
     register decode_PC(.out(decode_PC_out), .in(fetch_PC_out), .clk(not_clock), .en(1'b1), .clr(reset));
     register decode_A(.out(decode_A_out), .in(data_readRegA), .clk(not_clock), .en(1'b1), .clr(reset));
     register decode_B(.out(decode_B_out), .in(data_readRegB), .clk(not_clock), .en(1'b1), .clr(reset));
-    register decode_INSN(.out(decode_INSN_out), .in(decode_INSN_in), .clk(not_clock), .en(1'b1), .clr(reset));
+    register decode_INSN(.out(decode_INSN_out), .in(fetch_INSN_out), .clk(not_clock), .en(1'b1), .clr(reset));
     ////////// START OF EXECUTE //////////
 
     /// MUX for B input
-    wire need_reg_B;
-    assign need_reg_B = !(|decode_INSN_out[31:27]) || !(|(decode_INSN_out[31:27]^5'b00110));
+    wire is_R_type;
+    assign is_R_type = !(|decode_INSN_out[31:27]);
     wire [31:0] sign_extend_immed_out, ALU_input_B;
     sign_extend_17_32 extender(.out(sign_extend_immed_out), .in(decode_INSN_out[16:0]));
-    assign ALU_input_B = need_reg_B ? decode_B_out : sign_extend_immed_out;
+    assign ALU_input_B = is_R_type ? decode_B_out : sign_extend_immed_out;
 
     // ALU opcode -> account for addi insn needing opcode to be 00000.
-    wire [4:0] execute_alu_opc, not_I_type_opc;
-    wire is_I_type = !(|(decode_INSN_out[31:27]^5'b00101)) || !(|(decode_INSN_out[31:27]^5'b00111)) || !(|(decode_INSN_out[31:27]^5'b01000));
-    assign not_I_type_opc = !(|(decode_INSN_out[31:27]^5'b00110)) ? 5'd1 : 5'd0; // BLT logic case
-    assign execute_alu_opc = is_I_type ? not_I_type_opc : decode_INSN_out[6:2];
+    wire [4:0] execute_alu_opc;
+    assign execute_alu_opc = |(decode_INSN_out[31:27]^5'b00101) ? decode_INSN_out[6:2] : 5'd0;
 
     // ALU; NOTE: isLT, isNE are unused. OVF undefined too but that's ok?
     wire [31:0] alu_out;
@@ -140,7 +127,7 @@ module processor(
 
     // branching: destination calculation
     wire [31:0] pc_plus_N, pc_T;
-    alu_cla_outer pc_plus_N_alu(.data_operandA(decode_PC_out), .data_operandB(sign_extend_immed_out), .Cin(1'b0), .data_result(pc_plus_N), .Cout());
+    alu_cla_outer pc_plus_N_alu(.data_operandA(decode_PC_out), .data_operandB(sign_extend_immed_out), .Cin(1'b1), .data_result(pc_plus_N), .Cout());
     assign pc_T[26:0] = decode_INSN_out[26:0];
     assign pc_T[31:27] = {5{decode_INSN_out[26]}};
 
@@ -148,7 +135,7 @@ module processor(
     wire [4:0] decode_out_opcode;
     wire take_pc_N, take_T, take_rd;
     assign decode_out_opcode = decode_INSN_out[31:27]; 
-    assign take_pc_N = (!(|(decode_out_opcode^5'b00010)) && (alu_isNE)) || (!(|(decode_out_opcode^5'b00110)) && !(alu_isLT));
+    assign take_pc_N = (!(|(decode_out_opcode^5'b00010)) && (alu_isNE)) || (!(|(decode_out_opcode^5'b00110)) && (alu_isLT));
     assign take_T = (!(|(decode_out_opcode^5'b00011))) || (!(|(decode_out_opcode^5'b10110)) && (|(decode_B_out)));
     assign take_rd = (!(|(decode_out_opcode^5'b00100)));
 
@@ -156,6 +143,7 @@ module processor(
     wire [1:0] pc_branch_control;
     assign pc_branch_control[0] = take_pc_N || take_rd;
     assign pc_branch_control[1] = take_T || take_rd;
+
 
 
     ////////// END OF EXECUTE //////////
